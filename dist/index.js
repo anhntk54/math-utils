0da85eba68aedd4e27a4f3c24a8040d535752935
anhnhu <anhnt@konistudio.xyz>
2025-06-17 14:32:06 +0700

    begin

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
+// src/index.ts
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+        throw new Error('Inputs must be numbers');
+    return a + b;
+}
+function subtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+        throw new Error('Inputs must be numbers');
+    return a - b;
+}
+function multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+        throw new Error('Inputs must be numbers');
+    return a * b;
+}
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+        throw new Error('Inputs must be numbers');
+    if (b === 0)
+        throw new Error('Division by zero');
+    return a / b;
+}
