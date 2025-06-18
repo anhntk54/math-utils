4a5cf05714acd37e2ad9ae7804f58cc5c32dc0d9
anhnhu <anhnt@konistudio.xyz>
2025-06-18 14:30:48 +0700

    fix: correct addition logic in add function to include an increment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 export function add(a: number, b: number): number {
   if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Inputs must be numbers');
-  return a + b + b + a ;
+  return a + b + b + a  + 1;
 }
 
 export function subtract(a: number, b: number): number {
