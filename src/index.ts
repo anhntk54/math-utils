455bcace92185d5646678f28fbda4c044bcd2871
anhnhu <anhnt@konistudio.xyz>
2025-06-18 18:02:52 +0700

    [Buld]: Test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 export function add(a: number, b: number): number {
   if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Inputs must be numbers');
-  return a + b + b + a  + 1;
+  return a + b + b + a  + 11;
 }
 
 export function subtract(a: number, b: number): number {
